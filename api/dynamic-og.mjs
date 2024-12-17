bbf54076779068495eaea8c1682cce45133857df
Liran Tal <liran.tal@gmail.com>
2024-12-17 13:20:42 +0200

    feat: enable dynamic og and personalized invite link
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

---
+++ api/dynamic-og.mjs
@@ -0,0 +1,46 @@
+import { readFileSync } from "fs";
+import sharp from "sharp";
+import opentype from "opentype.js";
+
+const ShareCardSVG = readFileSync("./api/_share-card.svg", "utf8");
+
+function textToPath(text, x, y, fontSize) {
+  const font = opentype.loadSync("./fonts/Jersey10-Regular.ttf"); // Load your font file
+  const path = font.getPath(text, x, y, fontSize);
+  return path.toSVG();
+}
+
+export async function GET(request) {
+  const requestUrl = new URL(request.url);
+  const queryString = new URLSearchParams(requestUrl.search);
+  const text = queryString.get("score");
+
+  let score = Number(text);
+
+  if (!Number.isInteger(score)) {
+    score = 0;
+  }
+
+  const x = 100;
+  const y = 100;
+  const fontSize = 34;
+
+  let pathData = textToPath(score.toString(), x, y, fontSize);
+
+  // prefix is the '<path' part of the path data
+  // suffix is the rest of the path data
+  const prefix = pathData.substring(0, 5);
+  const suffix = pathData.substring(5);
+
+  // between them we add the fill and stroke attributes
+  pathData = `${prefix} fill="#FFFFFF" transform="translate(500, 30) scale(3)" ${suffix}`;
+
+  const svgCustomText = ShareCardSVG.replace("<TEMPLATE/>", pathData);
+
+  const image = await sharp(Buffer.from(svgCustomText)).toBuffer();
+  return new Response(image, {
+    headers: {
+      "Content-Type": "image/webp",
+    },
+  });
+}
---
+++ api/invite.mjs
@@ -0,0 +1,17 @@
+import { readFileSync } from "fs";
+
+const htmlTemplate = readFileSync("./api/_invite.html", "utf8");
+
+export default function handler(request, response) {
+  const { query } = request;
+  let score = Number(query.score);
+
+  if (!Number.isInteger(score)) {
+    score = 0;
+  }
+
+  const htmlString = htmlTemplate.replace("__SCORE__", score);
+
+  response.setHeader("Content-Type", "text/html; charset=utf-8");
+  return response.status(200).send(htmlString);
+}
